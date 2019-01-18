d29671d657d6199e99a0da65a73728f24e8de1b4
qscgy <sam.ehrenstein@gmail.com>
2019-01-18 15:15:23 -0500

    use adaptive threshold in shape detector

--- shapedetector.py
+++ shapedetector.py
@@ -17,25 +17,39 @@ class ShapeDetector:
             shape = 'triangle'
         elif len(approx)==4:    # could be square or line
             (x, y, w, h) = cv2.boundingRect(approx)
+            ar = w/float(h)
             area = cv2.contourArea(c)
-            if area/float(w*h) >=0.4:
-                shape = 'square'
-            else:
+            if area/float(w*h) <= 0.4 or ar <= 0.9 or ar >= 1.1:
                 shape = 'line'
+            else:
+                shape = 'square'
         else:
             shape = 'circle'    # shapes can only be square, triangle, line, or circle
 
         return shape
 
 
+def add_shape(shape, d):
+    if shape in d:
+        d[shape] = int(d[shape])+1
+    else:
+        d[shape] = 1
+
+
 def detect_shapes():
-    image = cv2.imread('/home/sam/Documents/shapes.jpg', cv2.IMREAD_COLOR)
+    image = cv2.imread('shapes.png', cv2.IMREAD_COLOR)
     resized = imutils.resize(image, width=300)  # resize to simplify shapes
     ratio = image.shape[0] / float(resized.shape[0])
 
     gray = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
     blurred = cv2.GaussianBlur(gray, (5, 5), 0)
-    thresh = cv2.threshold(blurred, 60, 255, cv2.THRESH_BINARY)[1]
+
+    # Here we use an adaptive threshold on the image, since we expect the lighting to be non-uniform.
+    thresh = cv2.adaptiveThreshold(blurred, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 5, 0)
+    # cv2.imshow("thresh", thresh)
+    # cv2.waitKey(0)
+
+    num_shapes = {}
 
     cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[1]
     sd = ShapeDetector()
@@ -46,14 +60,19 @@ def detect_shapes():
         cx = int((M["m10"] / M["m00"]) * ratio)
         cy = int((M["m01"] / M["m00"]) * ratio)
         shape = sd.detect(c)
+        add_shape(shape, num_shapes)
 
         c = c.astype(float)
         c = c*ratio
         c = c.astype(int)
-        cv2.drawContours(image, [c], -1, (0, 255, 0), 2)
-        cv2.putText(image, shape, (cx, cy), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+        cv2.drawContours(image, [c], -1, (255, 0, 0), 2)
+        cv2.putText(image, shape, (cx, cy), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2)
 
         cv2.imshow("Image", image)
         cv2.waitKey(0)
 
-detect_shapes()
+    return num_shapes
+
+
+num_shapes = detect_shapes()
+print(num_shapes)
