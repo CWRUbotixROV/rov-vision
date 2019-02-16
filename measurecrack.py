6881c8389bc96fb1ee38e61a86c74fbf70a7e90c
ryankarpuszka <rykarp5@gmail.com>
2019-02-15 21:48:35 -0500

    Find intersections of grid lines

--- measurecrack.py
+++ measurecrack.py
@@ -44,24 +44,27 @@ def measure_crack():
     	return (width/height)*1.85
 
 def measure_crack2():
-    image = cv2.imread('crack3.jpg', cv2.IMREAD_COLOR)
+    image = cv2.imread('crack4.jpg', cv2.IMREAD_COLOR)
     
-    # Convert BGR to HSV
-    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-
-    # define range of blue color in HSV
+    # define range of color in HSV
     lower = np.array([0,0,0])
     upper = np.array([180,50,100])
 
-    # Threshold the HSV image to get only blue colors
+    
+    
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    blurred = cv2.GaussianBlur(gray, (21, 21), 0)
+    blurredcolor = cv2.GaussianBlur(image, (5, 5), 0)
+    
+    # Convert BGR to HSV
+    hsv = cv2.cvtColor(blurredcolor, cv2.COLOR_BGR2HSV)
+    
+    # Threshold the HSV image to get only black colors
     mask = cv2.inRange(hsv, lower, upper)
     
     resized = imutils.resize(mask, width=300)
     cv2.imshow("mask", resized)
     cv2.waitKey(0)
-    
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    blurred = cv2.GaussianBlur(gray, (21, 21), 0)
 
     # Here we use an adaptive threshold on the image, since we expect the lighting to be non-uniform.
     thresh = cv2.adaptiveThreshold(blurred, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 5, 0)
@@ -75,11 +78,37 @@ def measure_crack2():
     cv2.imshow("edges", resized)
     cv2.waitKey(0)
     
-    lines = cv2.HoughLines(mask, 1, np.pi/180, 500)
+    lines = cv2.HoughLines(mask, 1, np.pi/180, 700)
     a,b,c = lines.shape
+    
+    top = []
+    bottom = []
+    left = []
+    right = []
+    
+    height, width, channels = image.shape
+    
     for i in range(a):
         rho = lines[i][0][0]
         theta = lines[i][0][1]
+        
+        #Sort lines
+        if (theta < 0.2):
+            if (rho < 0.25 * width):
+                left.append([rho, theta])
+            elif (rho > 0.75 * width):
+                right.append([rho, theta])
+        elif (theta > 2.94):
+            if (rho > -0.25 * width):
+                left.append([rho, theta])
+            elif (rho < -0.75 * width):
+                right.append([rho, theta])
+        elif (theta > 1.37 and theta < 1.77):
+            if (rho < 0.25 * height):
+                top.append([rho, theta])
+            elif (rho > 0.75 * height):
+                bottom.append([rho, theta])
+        
         a = np.cos(theta)
         b = np.sin(theta)
         x0 = a * rho
@@ -89,9 +118,25 @@ def measure_crack2():
         x2 = int(x0 - 10000*(-b))
         y2 = int(y0 - 10000*(a))
         #print(str(x1) + "," + str(y1) + "," + str(x2) + "," + str(y2))
-        cv2.line(image, (x1, y1), (x2, y2), (0, 255, 0), 5)
+        #cv2.line(image, (x1, y1), (x2, y2), (0, 255, 0), 5)
     
-    resized = imutils.resize(image, width=300)
+    print(left)
+    print(right)
+    print(top)
+    print(bottom)
+    
+    x1, y1 = intersect(top, left)
+    x2, y2 = intersect(top, right)
+    x3, y3 = intersect(bottom, left)
+    x4, y4 = intersect(bottom, right)
+    
+    cv2.line(image, (x1, y1), (x2, y2), (0, 255, 0), 5)
+    cv2.line(image, (x1, y1), (x3, y3), (0, 255, 0), 5)
+    cv2.line(image, (x2, y2), (x4, y4), (0, 255, 0), 5)
+    cv2.line(image, (x3, y3), (x4, y4), (0, 255, 0), 5)
+    
+    
+    resized = imutils.resize(image, width=500)
     cv2.imshow("lines", resized)
     cv2.waitKey(0)
     
@@ -103,5 +148,34 @@ def measure_crack2():
     #cv2.waitKey(0)
     
     return 0
+    
+def intersect(lines1, lines2):
+    xs = []
+    ys = []
+    for line1 in lines1:
+        a1 = np.cos(line1[1])
+        b1 = np.sin(line1[1])
+        m1 = -a1 / b1
+        x1 = a1 * line1[0]
+        y1 = b1 * line1[0]
+        b1 = y1 - (x1 * m1)
+        for line2 in lines2:
+            if (line2[1] == 0.0):
+                line2[1] = 0.0001
+            a2 = np.cos(line2[1])
+            b2 = np.sin(line2[1])
+            m2 = -a2 / b2
+            x2 = a2 * line2[0]
+            y2 = b2 * line2[0]
+            b2 = y2 - (x2 * m2)
+            x = (b1 - b2) / (m2 - m1)
+            y = m1 * x + b1
+            xs.append(x)
+            ys.append(y)
+    meanx = int(sum(xs) / len(xs))
+    meany = int(sum(ys) / len(ys))
+    return meanx, meany
+            
 
 print(measure_crack2())
+
