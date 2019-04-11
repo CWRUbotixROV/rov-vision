853dc84743c116522d3b1a8f1b53a9382c370293
Sam Ehrenstein <sam.ehrenstein@gmail.com>
2019-04-11 16:11:49 -0400

    clean up

--- dam_autonomy.py
+++ dam_autonomy.py
@@ -10,6 +10,12 @@ RC_CHAN_YAW = 4
 RC_CHAN_FORWARD = 5
 RC_CHAN_LATERAL = 6
 
+# PWM values
+UP_PWM = 1550
+DOWN_PWM = 1450
+LEFT_PWM = 1450
+RIGHT_PWM = 1550
+
 # mavproxy.py --master=udpin:192.168.2.1:14540 --out=udpout:192.168.2.2:14540
 
 
@@ -22,27 +28,30 @@ def set_rc_channel_pwm(id, pwm=1500):
     """
     if id < 1:
         return
-    if id < 9:
+    if id < 9:  # don't set values for channels above 8, since those are used for other things
         rc_channel_values = [65535 for _ in range(8)]
         rc_channel_values[id-1] = pwm
         master.mav.rc_channels_override_send(master.target_system, master.target_component, *rc_channel_values)
 
 
 def go_down():
-    set_rc_channel_pwm(RC_CHAN_THROTTLE, 1450)  # half speed, I think
+    set_rc_channel_pwm(RC_CHAN_THROTTLE, pwm=DOWN_PWM)
 
 def go_up():
-    set_rc_channel_pwm(RC_CHAN_THROTTLE, 1550)
+    set_rc_channel_pwm(RC_CHAN_THROTTLE, pwm=UP_PWM)
 
 def go_left():
-    set_rc_channel_pwm(RC_CHAN_LATERAL, 1450)
+    set_rc_channel_pwm(RC_CHAN_LATERAL, pwm=LEFT_PWM)
 
 def go_right():
-    set_rc_channel_pwm(RC_CHAN_LATERAL, 1550)
+    set_rc_channel_pwm(RC_CHAN_LATERAL, pwm=RIGHT_PWM)
 
 def stop():
     for i in range(6):
         set_rc_channel_pwm(i+1, 1500)
+
+def disarm(_master):
+    _master.mav.command_long_send(1, 1, 400, 0, 0, 0, 0, 0, 0, 0, 0) # disarm
 
 master = mavutil.mavlink_connection('udpin:192.168.2.1:14540')
 # p = subprocess.Popen(['python3', 'follow_line.py'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -72,9 +81,9 @@ try:
             go_right()
         else:
             stop()
-        time.sleep(tn - int(round(time.time()*1000)))
+        time.sleep(tn - int(round(time.time()*1000)))   # ensure that we update the direction at a constant rate
 except KeyboardInterrupt:
     pass
 
 stop()
-master.mav.command_long_send(1, 1, 400, 0, 0, 0, 0, 0, 0, 0, 0) # disarm
+disarm(master)
