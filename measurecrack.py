6018793d13dab725ef6c920f97eb0b5a6f5281b1
ryankarpuszka <rykarp5@gmail.com>
2019-04-13 14:18:33 -0400

    Revert to ratio method if grid line detection fails

--- measurecrack.py
+++ measurecrack.py
@@ -1,16 +1,18 @@
 import cv2, imutils
 import numpy as np
 
+# Measures crack using raio method
 def measure_crack(image):
-    #image = cv2.imread('crack3.jpg', cv2.IMREAD_COLOR)
-    
+    #Get dimensions of blue rectangle
     width, height = blue_rectangle(image)
     
+    # Return larger dimension. Multiply ratio by 1.85
     if height > width:
-        return (height/width)*1.85
+        return (height/width)*1.9
     else:
-        return (width/height)*1.85
-    	
+        return (width/height)*1.9
+
+# Finds dimensions of a blue rectangle    	
 def blue_rectangle(image):
     # Convert BGR to HSV
     hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
@@ -22,18 +24,9 @@ def blue_rectangle(image):
     # Threshold the HSV image to get only blue colors
     mask = cv2.inRange(hsv, lower_blue, upper_blue)
     
-    # Bitwise-AND mask and original image
-    #blue = cv2.bitwise_and(image, image, mask= mask)
-    
-    #cv2.imshow("mask", mask)
-    #cv2.waitKey(0)
-    
     # Find contours
     contours, hierarchy = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     img = cv2.drawContours(mask, contours, -1, (50,255,0), 3)
-    resized = imutils.resize(img, width=300)
-    cv2.imshow("contours", resized)
-    cv2.waitKey(0)
     
     # Find max area contour
     maxArea = 0
@@ -49,17 +42,13 @@ def blue_rectangle(image):
     height = rect[1][1]
     return width, height
 
+# Measures crack using perspective transformation method
 def measure_crack2(image):
-    #image = cv2.imread('crack5.jpg', cv2.IMREAD_COLOR)
-    
     # define range of color in HSV
     lower = np.array([0,0,0])
     upper = np.array([180,50,100])
 
-    
-    
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    blurred = cv2.GaussianBlur(gray, (21, 21), 0)
+    #Gaussian Blur
     blurredcolor = cv2.GaussianBlur(image, (5, 5), 0)
     
     # Convert BGR to HSV
@@ -68,17 +57,15 @@ def measure_crack2(image):
     # Threshold the HSV image to get only black colors
     mask = cv2.inRange(hsv, lower, upper)
     
-    resized = imutils.resize(mask, width=600)
-    cv2.imshow("mask", resized)
-    cv2.waitKey(0)
+    # resized = imutils.resize(mask, width=600)
+    # cv2.imshow("mask", resized)
+    # cv2.waitKey(0)
     
-    # Canny line detection
-    edges = cv2.Canny(gray, 50, 150, apertureSize=3)
-    resized = imutils.resize(edges, width=600)
-    #cv2.imshow("edges", resized)
-    #cv2.waitKey(0)
-    
+    # Detect lines
     lines = cv2.HoughLines(mask, 1, np.pi/180, 700)
+    # If no lines are detected, revert to ratio method
+    if (lines is None):
+        return measure_crack(image)
     a,b,c = lines.shape
     
     top = []
@@ -92,7 +79,7 @@ def measure_crack2(image):
         rho = lines[i][0][0]
         theta = lines[i][0][1]
         
-        #Sort lines
+        # Sort lines into left, right, top, and bottom
         if (theta < 0.2):
             if (rho < 0.25 * width):
                 left.append([rho, theta])
@@ -108,55 +95,55 @@ def measure_crack2(image):
                 top.append([rho, theta])
             elif (rho > 0.75 * height):
                 bottom.append([rho, theta])
-        
-        a = np.cos(theta)
-        b = np.sin(theta)
-        x0 = a * rho
-        y0 = b * rho
-        x1 = int(x0 + 10000*(-b))
-        y1 = int(y0 + 10000*(a))
-        x2 = int(x0 - 10000*(-b))
-        y2 = int(y0 - 10000*(a))
-        #print(str(x1) + "," + str(y1) + "," + str(x2) + "," + str(y2))
-        #cv2.line(image, (x1, y1), (x2, y2), (0, 255, 0), 5)
     
-    #print(left)
-    #print(right)
-    #print(top)
-    #print(bottom)
-    
+    # If grid line detection fails, revert to ratio method
+    if (len(top) == 0 or len(bottom) == 0 or len(left) == 0 or len(right) == 0):
+        return measure_crack(image)
+
     x1, y1 = intersect(top, left)
     x2, y2 = intersect(top, right)
     x3, y3 = intersect(bottom, left)
     x4, y4 = intersect(bottom, right)
     
-    cv2.line(image, (x1, y1), (x2, y2), (0, 255, 0), 5)
-    cv2.line(image, (x1, y1), (x3, y3), (0, 255, 0), 5)
-    cv2.line(image, (x2, y2), (x4, y4), (0, 255, 0), 5)
-    cv2.line(image, (x3, y3), (x4, y4), (0, 255, 0), 5)
+    # Draw lines on image
+    # cv2.line(image, (x1, y1), (x2, y2), (0, 255, 0), 5)
+    # cv2.line(image, (x1, y1), (x3, y3), (0, 255, 0), 5)
+    # cv2.line(image, (x2, y2), (x4, y4), (0, 255, 0), 5)
+    # cv2.line(image, (x3, y3), (x4, y4), (0, 255, 0), 5)
     
+    # Display image with lines drawn
+    # resized = imutils.resize(image, width=600)
+    # cv2.imshow("lines", resized)
+    # cv2.waitKey(0)
     
-    resized = imutils.resize(image, width=600)
-    cv2.imshow("lines", resized)
-    cv2.waitKey(0)
-    
+    # Find perspective transform matrix
     src = np.float32([[x1, y1], [x2, y2], [x3, y3], [x4, y4]])
     dst = np.float32([[0, 0], [3000, 0], [0, 3000], [3000, 3000]])
     matrix = cv2.getPerspectiveTransform(src, dst)
     
+    # Perspective transform original image
     warped = cv2.warpPerspective(image, matrix, (3000, 3000))
     
-    resized = imutils.resize(warped, width=600)
-    cv2.imshow("warped", resized)
-    cv2.waitKey(0)
+    # Display warped image
+    # resized = imutils.resize(warped, width=600)
+    # cv2.imshow("warped", resized)
+    # cv2.waitKey(0)
     
     width, height = blue_rectangle(warped)
     
+    # Find larger dimension. Divide by 100 because 100 pixels = 1 cm.
     if (width > height):
-        return width / 100
+        ret = width / 100
     else:
-        return height / 100
+        ret = height / 100
     
+    # If dimension is out of bounds, revert to ratio method
+    if (ret > 21 or ret < 7):
+        return measure_crack(image)
+    else:
+        return ret
+
+# Find the average intersection point between two sets of lines    
 def intersect(lines1, lines2):
     xs = []
     ys = []
@@ -183,10 +170,6 @@ def intersect(lines1, lines2):
     meanx = int(sum(xs) / len(xs))
     meany = int(sum(ys) / len(ys))
     return meanx, meany
-            
-image = cv2.imread('crack4.jpg', cv2.IMREAD_COLOR)
-print(measure_crack(image))
-print(measure_crack2(image))
 
 
 
