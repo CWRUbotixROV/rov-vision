8f61f9ea4d9e7b198239962e7100361d8aa07d9b
ryankarpuszka <rykarp5@gmail.com>
2019-05-10 22:39:56 -0400

    Display grid when it reaches end of line

--- grid_map.py
+++ grid_map.py
@@ -5,7 +5,7 @@ import math
 class GridMap:
     hlines = []
     vlines = []
-    thresh = 200
+    thresh = 300
     crackx = 0
     cracky = 0
     maxblueratio = 0
@@ -17,16 +17,9 @@ class GridMap:
     def update(self, image):
         blurred = cv2.GaussianBlur(image, (5, 5), 0)
         hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
-        LOWER = np.array([60, 0, 0])
-        UPPER = np.array([130, 255, 45])
+        LOWER = np.array([0, 0, 0])
+        UPPER = np.array([180, 100, 110])
         mask = cv2.inRange(hsv, LOWER, UPPER)
-
-        LOWER = np.array([0, 14, 9])
-        UPPER = np.array([2, 25, 17])
-        mask = cv2.inRange(blurred, LOWER, UPPER)
-
-        gray = cv2.cvtColor(blurred, cv2.COLOR_BGR2GRAY)
-        #thresh = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 11, 5)
 
         # Detect Lines
         lines = cv2.HoughLinesP(mask, 1, np.pi / 180, 50, None, 50, 10)
@@ -35,19 +28,21 @@ class GridMap:
         horizontal = []
         vertical = []
 
+        HORIZONTAL_THRESH = 0.5
+        VERTICAL_THRESH = 2
+
         #Sort lines by horizontal and vertical
         if lines is not None:
             for i in range(0, len(lines)):
                 l = lines[i][0]
                 if (l[2] != l[0]):
                     m = (l[3] - l[1]) / (l[2] - l[0])
-                    if (m > -0.5 and m < 0.5):
+                    if (m > -HORIZONTAL_THRESH and m < HORIZONTAL_THRESH):
                         y = ((width / 2) - l[0]) * m + l[1]
                         horizontal.append(y)
-                    elif (m < -2 or m > 2):
+                    elif (m < -VERTICAL_THRESH or m > VERTICAL_THRESH):
                         x = ((height / 2) - l[1]) * (1 / m) + l[0]
                         vertical.append(x)
-                #cv2.line(image, (l[0], l[1]), (l[2], l[3]), (0,255,0), 3, cv2.LINE_AA)
 
         #Sort lines
         horizontal.sort()
@@ -70,12 +65,13 @@ class GridMap:
             avg = sum / count
             havg.append(avg)
         
-        # Group vertical lines that are close together into vavg
+        # Sort lines
         vertical.sort()
         sum = 0
         count = 0
         vavg = []
 
+        # Group vertical lines that are close together into vavg
         if len(vertical) > 0:
             for i in range(0, len(vertical) - 1):
                 sum += vertical[i]
@@ -89,34 +85,8 @@ class GridMap:
             count += 1
             avg = sum / count
             vavg.append(avg)
-        
 
-        #Update lines
-        # for y in havg:
-        #     cv2.line(image, (0, int(y)), (10000, int(y)), (0,255,0), 3, cv2.LINE_AA)
-        #     update = False
-        #     for line in self.hlines:
-        #         if (abs(line[0] - y) < thresh):
-        #             line[0] = y
-        #             line[1] += 1
-        #             update = True
-        #     if (update == False):
-        #         self.hlines.append([y, 0])
-        # for x in vavg:
-        #     cv2.line(image, (int(x), 0), (int(x), 10000), (0,255,0), 3, cv2.LINE_AA)
-
-        #Update lines
-        for y in havg:
-            cv2.line(image, (0, int(y)), (10000, int(y)), (255,0,0), 3, cv2.LINE_AA)
-        #     update = False
-        #     for line in self.hlines:
-        #         if (line.update(y)):
-        #             update = True
-        #         elif (line.unupdated > 5):
-        #             self.hlines.remove(line)
-        #     if (update == False):
-        #         self.hlines.append(Line(y, 500))
-
+        # Update horizontal and vertical lines with new coordinates
         updateLines(havg, self.hlines, height / 2)
         updateLines(vavg, self.vlines, width / 2)
 
@@ -126,7 +96,7 @@ class GridMap:
                 self.y += line.crossed
                 line.counted = True
             y = line.pos
-            cv2.line(image, (0, int(y)), (10000, int(y)), (0,255,0), 3, cv2.LINE_AA)
+            #cv2.line(image, (0, int(y)), (10000, int(y)), (0,255,0), 3, cv2.LINE_AA)
 
         # Check if any vertical lines have crossed
         for line in self.vlines:
@@ -134,59 +104,56 @@ class GridMap:
                 self.x += line.crossed
                 line.counted = True
             x = line.pos
-            cv2.line(image, (int(x), 0), (int(x), 10000), (0, 255, 0), 3, cv2.LINE_AA)
+            #cv2.line(image, (int(x), 0), (int(x), 10000), (0, 255, 0), 3, cv2.LINE_AA)
 
         # Find the ratio of the image that is blue
         blueratio = blueRectangle(image)
-        # If the blue ratio is bigger than the max then update the location of the blue crack
-        if (blueratio > self.maxblueratio):
+        # If the blue ratio is bigger than the max and current position is within bounds then update the location of the blue crack
+        if (blueratio > self.maxblueratio and self.x >= 0 and self.x <=3 and self.y >=0 and self.y <=2):
             self.maxblueratio = blueratio
+            self.crackimage = image
             self.crackx = self.x
             self.cracky = self.y
 
-        # lines = cv2.HoughLines(mask, 1, np.pi / 180, 300, None, 0, 0)
-        # if lines is not None:
-        #     for i in range(0, len(lines)):
-        #         rho = lines[i][0][0]
-        #         theta = lines[i][0][1]
-        #         a = math.cos(theta)
-        #         b = math.sin(theta)
-        #         x0 = a * rho
-        #         y0 = b * rho
-        #         pt1 = (int(x0 + 10000*(-b)), int(y0 + 10000*(a)))
-        #         pt2 = (int(x0 - 10000*(-b)), int(y0 - 10000*(a)))
-        #         cv2.line(image, pt1, pt2, (0,255,0), 3, cv2.LINE_AA)
+        if (self. x == 4 or self.y == 3 or (self.x == 3 and self.y == -1)):
+            length = 0
+            displayCrack(self.crackx, self.cracky, length)
 
         resized = imutils.resize(mask, width=800)
 
-        cv2.imshow("mask", resized)
-        cv2.waitKey(1)
+        # cv2.imshow("mask", resized)
+        # cv2.waitKey(1)
 
-        resized = imutils.resize(image, width=800)
+        # resized = imutils.resize(image, width=800)
 
-        cv2.imshow("image", resized)
-        cv2.waitKey(1)
+        # cv2.imshow("image", resized)
+        # cv2.waitKey(0)
 
 def updateLines(newLines, lines, half):
+    # Check each coordinate against each existing line
     for coordinate in newLines:
         update = False
         for line in lines:
             if (line.update(coordinate)):
                 update = True
                 break
+        # If a coordinate does not correspond to any existing line, create a new line
         if (update == False):
             lines.append(Line(coordinate, half))
+    # Check to see which lines have not updated with any of the new coordinates
+    REMOVE_THRESH = 5
     for line in lines:
         if (line.updated == False):
             line.unupdated += 1
-            if (line.unupdated > 5):
+            # If a line has not been updated, remove it
+            if (line.unupdated > REMOVE_THRESH):
                 lines.remove(line)
         else:
             line.updated = False
 
 
 class Line:
-    thresh = 200
+    thresh = 300
     def __init__(self, pos, bound):
         self.pos = pos
         self.duration = 1
@@ -195,19 +162,18 @@ class Line:
         self.bound = bound
         self.crossed = 0
         self.counted = False
-        self.start = pos
     
     def update(self, pos):
+        '''Checks if the line is close to the new coordinate and returns true if it updates'''
+        # Check if coordinate is within threshold of current position
         if (abs(self.pos - pos) < self.thresh):
-            
+            # If it has not crossed yet, check to see it crosses on this update
             if (self.duration > 0 and self.crossed == 0):
                 if (pos >= self.bound and self.pos < self.bound):
                     self.crossed = -1
-                    print("-1")
                 elif (pos <= self.bound and self.pos > self.bound):
                     self.crossed = 1
-                    print("1")
-
+            # Update position and updated status
             self.pos = pos
             self.duration += 1
             self.updated = True
@@ -217,7 +183,9 @@ class Line:
             return False
 
 def blueRectangle(image):
-    #hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
+    '''Returns ratio of the image that is blue'''
+
+    # Mask blue colors
     LOWER_BLUE = np.array([0, 0, 0])
     UPPER_BLUE = np.array([255, 80, 80])
     mask = cv2.inRange(image, LOWER_BLUE, UPPER_BLUE)
@@ -235,11 +203,12 @@ def blueRectangle(image):
 
 
 def displayCrack(x, y, length):
+    '''Displays the length of the crack in the square at x, y'''
     CELL_SIZE = 200
     PADDING = 20
     HEIGHT = CELL_SIZE * 3 + (2 * PADDING)
     WIDTH = CELL_SIZE * 4 + (2 * PADDING)
-    THICKNESS = 3
+    THICKNESS = 2
 
     # Create white image
     image = np.zeros((HEIGHT, WIDTH, 3), np.uint8)
@@ -251,16 +220,19 @@ def displayCrack(x, y, length):
     for n in range(4):
         cv2.line(image, (PADDING, CELL_SIZE * n + PADDING), (CELL_SIZE * 4 + PADDING, CELL_SIZE * n + PADDING), (0, 0, 0), THICKNESS)
 
+    # Round length
+    length = round(length, 1)
+
     # Write text
-    cv2.putText(image, str(length) + " cm", (int(CELL_SIZE * (x + 0.3) + PADDING), int(CELL_SIZE * (y + 0.5) + PADDING)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), THICKNESS)
+    cv2.putText(image, str(length) + " cm", (int(CELL_SIZE * (x + 0.2) + PADDING), int(CELL_SIZE * (y + 0.5) + PADDING)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), THICKNESS)
 
     cv2.imshow("Crack map", image)
     cv2.waitKey(0)
 
-video = cv2.VideoCapture("/home/vm/Downloads/underwater.mp4")
-map = GridMap(0, 0)
-#video.set(cv2.CAP_PROP_POS_FRAMES, 700)
-#displayCrack(2, 1, 13.6)
+video = cv2.VideoCapture("/home/vm/Downloads/line.mp4")
+map = GridMap(0, -1)
+#video.set(cv2.CAP_PROP_POS_FRAMES, 780)
+#displayCrack(2, 2, 13.672)
 while (True):
     retval, image = video.read()
     map.update(image)
