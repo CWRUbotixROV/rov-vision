bdf2d6aaba1b663f5de69d2fd351b864cec5f9ef
vm <aranya_kumar@yahoo.com>
2019-06-21 11:05:28 -0400

    measures lengths of the cannon

--- cannonvolume.py
+++ cannonvolume.py
@@ -7,22 +7,24 @@ line = False # we gon draw a line or what? ayy lmao
 def clickAndMeasure(event, x, y, flags, param):
 # defines the length of the bump-stick
     global refPt
-    line = param[0]
+    #line = param[0]
 
     if event == cv2.EVENT_LBUTTONDOWN:
         refPt = [(x,y)] # a and b are first x and y coordinates for line segment
 
     elif event == cv2.EVENT_LBUTTONUP:
         refPt.append((x,y))
-
-print(refPt)
+        #print(refPt)
 
 def lineLengthMeasurement():
+    #print (refPt)
     x1,y1 = refPt[0]
     x2,y2 = refPt[1]
-    length = np.sqrt(((x2-x1)^2)+((y2-y1)^2))
+    length = np.sqrt(((x2-x1)**2)+((y2-y1)**2))
+    return length
 
 def imageRead():
+    global refPt
     image = cv2.imread('real_shapes.png', cv2.IMREAD_COLOR)
     cv2.namedWindow("image")
     clone = image.copy()
@@ -35,15 +37,15 @@ def imageRead():
             cv2.line(image,(refPt[0]),(refPt[1]),(0,255,0),thickness=3,lineType=8,shift=0)
 
         if key == ord("n"):
-            cv2.destroyAllWindows()
-            np.delete(refPt)
-            cannonVolume = imageRead()
+            refPt = []
+            image = clone
         elif key == ord("y"):
             break
 
     bumpStick = lineLengthMeasurement()
     print(bumpStick)
-    cv2.setMouseCallback("image", clickAndMeasure)
+    clone1 = image.copy()
+    cv2.setMouseCallback("image", clickAndMeasure, [clone1])
 
     while True:
         cv2.imshow('image', image)
@@ -52,24 +54,17 @@ def imageRead():
             cv2.line(image,(refPt[0]),(refPt[1]),(0,255,0),thickness=3,lineType=8,shift=0)
 
         if key == ord("n"):
-            cv2.destroyAllWindows()
-            np.delete(refPt)
-            cannonVolume = imageRead()
+            refPt = []
+            image = clone1
+            #cannonVolume = imageRead()
         elif key == ord("y"):
             break
-
-        radius1 = (1/2)*lineLengthMeasurement()
-    if len(refPt) == 2:
-        roi = clone[refPt[0][1]:refPt[1][0], refPt[0][0]:refPt[1][0]]
-        cv2.waitKey(0)
-
+    ratio = lineLengthMeasurement() / bumpStick
+    diameter1 = ratio * measure
+    radius1 = (0.5) * diameter1
+    print("Diameter: " + str(diameter1))
+    print("Radius: " + str(radius1))
+    
+    
+measure = float(input("First measurement: "))
 cannonVolume = imageRead()
-
-'''
-image = cv2.imread('real_shapes.png', cv2.IMREAD_COLOR)
-cv2.imshow('image', image)
-cv2.namedWindow("image")
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-print(refPt)
-'''
