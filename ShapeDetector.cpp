37e3f7a4194146e8b8eb7267f6aa187fe23387d8
Aranya Kumar <axk1067@case.edu>
2019-11-16 11:33:04 -0500

    imports some of the necessary classes

---
+++ ShapeDetector.cpp
@@ -0,0 +1,11 @@
+#include "ShapeDetector.h"
+#include <string>
+
+class ShapeDetector
+
+{ public:
+    std::string shape = "unidentified";
+    double cv2.arcLength(c,true);
+    void cv::GaussianBlur();
+
+}
