a59c3e36c24fa9b4cf9380dddd325f57821442c1
Austin Keppers <akeppers21@gmail.com>
2020-01-18 12:49:22 -0500

    Cleaned up code

--- vision/coral.cpp
+++ vision/coral.cpp
@@ -1,4 +1,3 @@
-
 #include <iostream>
 #include "opencv2/core.hpp"
 #include "opencv2/highgui.hpp"
@@ -15,12 +14,13 @@ const char* keys =
     "{ input1 | coral_before.png          | Path to input image 1. }"
     "{ input2 | coral_after.png | Path to input image 2. }";
 
-void calculateCoralContours(Mat img, vector<vector<Point> > *contours, vector<Vec4i> *hierarchy,
-	       	int cannyThreshold, Scalar lowColorThreshold, Scalar highColorThreshold, int minArcLength)
+// Calculate the the Contours for the coral and throw out contours that are too small
+void calculateCoralContours(Mat img, vector<vector<Point> > *contours, vector<Vec4i> *hierarchy, 
+		Scalar lowColorThreshold, Scalar highColorThreshold, int minArcLength, Size blurSize)
 {
     // Blur images
     Mat img_thresh;
-    blur(img, img_thresh, Size(8, 8));
+    blur(img, img_thresh, blurSize);
 
     // Create threshold 
     inRange(img_thresh, lowColorThreshold, highColorThreshold, img_thresh);
@@ -37,6 +37,45 @@ void calculateCoralContours(Mat img, vector<vector<Point> > *contours, vector<Ve
 	}
     }
     contours->resize(j);
+}
+
+// Find the contours in ContourImg and display the bounding rectangles in displayImg
+void drawContourBoundingRects(Mat contourImg, Mat displayImg, Scalar color, int thickness)
+{
+    vector<vector<Point>> contours;
+    vector<Vec4i> hierarchy;
+    findContours(contourImg, contours, hierarchy, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
+
+    // Display bounding boxes on after picture
+    for(size_t i = 0; i < contours.size(); i++)
+    {
+	    Rect boundRect = boundingRect(contours[i]);
+	    rectangle(displayImg, boundRect.tl(), boundRect.br(), color, thickness);
+    }
+}
+
+// Draw the contours to an image with only those contours, an image with the all the contours from that picture, and an image where the contours are expanded
+void drawMultipleContours(vector<vector<Point>> contours, vector<Vec4i> hierarchy, Mat exclusive, Mat total, Mat totalExpanded, int expandAmount)
+{
+    for( size_t i = 0; i< contours.size(); i++ )
+    {
+        drawContours(exclusive, contours, (int)i, Scalar(255), FILLED, LINE_8, hierarchy, 0 );
+        drawContours(total, contours, (int)i, Scalar(255), FILLED, LINE_8, hierarchy, 0 );
+        drawContours(totalExpanded, contours, (int)i, Scalar(255), FILLED, LINE_8, hierarchy, 0 );
+        drawContours(totalExpanded, contours, (int)i, Scalar(255), expandAmount, LINE_8, hierarchy, 0 );
+    }
+}
+
+// Apply a perspective transform to a list of contours
+void transformContours(Mat contourTransform, vector<vector<Point>>* contours)
+{
+    for(size_t i = 0; i < contours->size(); i++)
+    {
+    	vector<Point2f> originalPoints, projectedPoints;
+	Mat((*contours)[i]).copyTo(originalPoints);
+	perspectiveTransform(originalPoints,  projectedPoints, contourTransform);
+	Mat(projectedPoints).copyTo((*contours)[i]);
+    }
 }
 
 int main( int argc, char* argv[] )
@@ -91,8 +130,8 @@ int main( int argc, char* argv[] )
     Mat H = findHomography( coral_before, coral_after, RANSAC );
 
     // Find Contours in before and after pictures
-    int threshold = 50;
     int minArc = 30;
+    Size blurSize = Size(8, 8);
 
     Scalar lowBleach(175, 175, 175);
     Scalar highBleach(256, 256, 256);
@@ -102,10 +141,10 @@ int main( int argc, char* argv[] )
     vector<vector<Point> > contoursHealthy1, contoursHealthy2, contoursBleach1, contoursBleach2;
     vector<Vec4i> hierarchyHealthy1, hierarchyHealthy2, hierarchyBleach1, hierarchyBleach2;
 
-    calculateCoralContours(img1, &contoursBleach1, &hierarchyBleach1, threshold, lowBleach, highBleach, minArc);
-    calculateCoralContours(img2, &contoursBleach2, &hierarchyBleach2, threshold, lowBleach, highBleach, minArc);
-    calculateCoralContours(img1, &contoursHealthy1, &hierarchyHealthy1, threshold, lowHealthy, highHealthy, minArc);
-    calculateCoralContours(img2, &contoursHealthy2, &hierarchyHealthy2, threshold, lowHealthy, highHealthy, minArc);
+    calculateCoralContours(img1, &contoursBleach1, &hierarchyBleach1, lowBleach, highBleach, minArc, blurSize);
+    calculateCoralContours(img2, &contoursBleach2, &hierarchyBleach2, lowBleach, highBleach, minArc, blurSize);
+    calculateCoralContours(img1, &contoursHealthy1, &hierarchyHealthy1, lowHealthy, highHealthy, minArc, blurSize);
+    calculateCoralContours(img2, &contoursHealthy2, &hierarchyHealthy2, lowHealthy, highHealthy, minArc, blurSize);
 
     // Create masks for after picture
     Mat afterHealth = Mat::zeros(img2.size(), CV_8UC1);
@@ -113,50 +152,20 @@ int main( int argc, char* argv[] )
     Mat afterTotal = Mat::zeros(img2.size(), CV_8UC1);
     Mat afterTotalExpanded = Mat::zeros(img2.size(), CV_8UC1);
 
-    for( size_t i = 0; i< contoursHealthy2.size(); i++ )
-    {
-        drawContours(afterHealth, contoursHealthy2, (int)i, Scalar(255), FILLED, LINE_8, hierarchyHealthy2, 0 );
-        drawContours(afterTotal, contoursHealthy2, (int)i, Scalar(255), FILLED, LINE_8, hierarchyHealthy2, 0 );
-        drawContours(afterTotalExpanded, contoursHealthy2, (int)i, Scalar(255), FILLED, LINE_8, hierarchyHealthy2, 0 );
-        drawContours(afterTotalExpanded, contoursHealthy2, (int)i, Scalar(255), 20, LINE_8, hierarchyHealthy2, 0 );
-    }
-    for( size_t i = 0; i< contoursBleach2.size(); i++ )
-    {
-        drawContours(afterBleach, contoursBleach2, (int)i, Scalar(255), FILLED, LINE_8, hierarchyBleach2, 0 );
-        drawContours(afterTotal, contoursBleach2, (int)i, Scalar(255), FILLED, LINE_8, hierarchyBleach2, 0 );
-        drawContours(afterTotalExpanded, contoursBleach2, (int)i, Scalar(255), FILLED, LINE_8, hierarchyBleach2, 0 );
-        drawContours(afterTotalExpanded, contoursBleach2, (int)i, Scalar(255), 20, LINE_8, hierarchyBleach2, 0 );
-    }
-    
+    drawMultipleContours(contoursHealthy2, hierarchyHealthy2, afterHealth, afterTotal, afterTotalExpanded, 20);
+    drawMultipleContours(contoursBleach2, hierarchyBleach2, afterBleach, afterTotal, afterTotalExpanded, 20);
 
     // Transfrom before coral onto after picture and create masks 
-    Mat beforeHealthy = Mat::zeros(img2.size(), CV_8UC1);
+    Mat beforeHealth = Mat::zeros(img2.size(), CV_8UC1);
     Mat beforeBleach = Mat::zeros(img2.size(), CV_8UC1);
     Mat beforeTotal = Mat::zeros(img2.size(), CV_8UC1);
     Mat beforeTotalExpanded = Mat::zeros(img2.size(), CV_8UC1);
 
-    for(size_t i = 0; i < contoursHealthy1.size(); i++)
-    {
-    	vector<Point2f> originalPoints, projectedPoints;
-	Mat(contoursHealthy1[i]).copyTo(originalPoints);
-	perspectiveTransform(originalPoints,  projectedPoints, H);
-	Mat(projectedPoints).copyTo(contoursHealthy1[i]);
-    	drawContours(beforeHealthy, contoursHealthy1, int(i), Scalar(255), FILLED, LINE_8, hierarchyHealthy1, 0 );
-    	drawContours(beforeTotal, contoursHealthy1, int(i), Scalar(255), FILLED, LINE_8, hierarchyHealthy1, 0 );
-    	drawContours(beforeTotalExpanded, contoursHealthy1, int(i), Scalar(255), FILLED, LINE_8, hierarchyHealthy1, 0 );
-    	drawContours(beforeTotalExpanded, contoursHealthy1, int(i), Scalar(255), 20, LINE_8, hierarchyHealthy1, 0 );
-    }
-    for(size_t i = 0; i < contoursBleach1.size(); i++)
-    {
-    	vector<Point2f> originalPoints, projectedPoints;
-	Mat(contoursBleach1[i]).copyTo(originalPoints);
-	perspectiveTransform(originalPoints,  projectedPoints, H);
-	Mat(projectedPoints).copyTo(contoursBleach1[i]);
-    	drawContours(beforeBleach, contoursBleach1, int(i), Scalar(255), FILLED, LINE_8, hierarchyBleach1, 0 );
-    	drawContours(beforeTotal, contoursBleach1, int(i), Scalar(255), FILLED, LINE_8, hierarchyBleach1, 0 );
-    	drawContours(beforeTotalExpanded, contoursBleach1, int(i), Scalar(255), FILLED, LINE_8, hierarchyBleach1, 0 );
-    	drawContours(beforeTotalExpanded, contoursBleach1, int(i), Scalar(255), 20, LINE_8, hierarchyBleach1, 0 );
-    }
+    transformContours(H, &contoursHealthy1);
+    transformContours(H, &contoursBleach1);
+
+    drawMultipleContours(contoursHealthy1, hierarchyHealthy1, beforeHealth, beforeTotal, beforeTotalExpanded, 20);
+    drawMultipleContours(contoursBleach1, hierarchyBleach1, beforeBleach, beforeTotal, beforeTotalExpanded, 20);
     
     // Combine masks to create new masks for growth, damage, bleaching, and recovery
     Mat growth = Mat::zeros(img2.size(), CV_8UC1);
@@ -172,38 +181,15 @@ int main( int argc, char* argv[] )
 
     bitwise_and(afterHealth, beforeBleach, recovery);
 
-    bitwise_and(afterBleach, beforeHealthy, bleaching);
+    bitwise_and(afterBleach, beforeHealth, bleaching);
 
-    // Find contours in combined masks
-    vector<vector<Point>> growthContours, damageContours, recoveryContours, bleachingContours;
-    vector<Vec4i> growthHierarchy, damageHierarchy, recoveryHierarchy, bleachingHierarchy;
-    findContours(growth, growthContours, growthHierarchy, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
-    findContours(damage, damageContours, damageHierarchy, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
-    findContours(recovery, recoveryContours, recoveryHierarchy, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
-    findContours(bleaching, bleachingContours, bleachingHierarchy, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
+    // Find contours in combined masks and draw bounding boxes
+    int lineThickness = 2;
 
-    // Display bounding boxes on after picture
-    for(size_t i = 0; i < growthContours.size(); i++)
-    {
-	    Rect boundRect = boundingRect(growthContours[i]);
-	    rectangle(img2, boundRect.tl(), boundRect.br(), Scalar(0, 255, 0), 2);
-    }
-    for(size_t i = 0; i < damageContours.size(); i++)
-    {
-	    Rect boundRect = boundingRect(damageContours[i]);
-	    rectangle(img2, boundRect.tl(), boundRect.br(), Scalar(0, 255, 255), 2);
-    }
-    for(size_t i = 0; i < recoveryContours.size(); i++)
-    {
-	    Rect boundRect = boundingRect(recoveryContours[i]);
-	    rectangle(img2, boundRect.tl(), boundRect.br(), Scalar(255, 0, 0), 2);
-    }
-    for(size_t i = 0; i < bleachingContours.size(); i++)
-    {
-	    Rect boundRect = boundingRect(bleachingContours[i]);
-	    rectangle(img2, boundRect.tl(), boundRect.br(), Scalar(0, 0, 255), 2);
-    }
-
+    drawContourBoundingRects(growth, img2, Scalar(0, 255, 0), lineThickness);
+    drawContourBoundingRects(damage, img2, Scalar(0, 255, 255), lineThickness);
+    drawContourBoundingRects(recovery, img2, Scalar(255, 0, 0), lineThickness);
+    drawContourBoundingRects(bleaching, img2, Scalar(0, 0, 255), lineThickness);
 
     // Show the bounding boxes 
     imshow("Coral Changes", img2);
