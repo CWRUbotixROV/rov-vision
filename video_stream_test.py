670000fd7cd8be2a263581514c452fcc9dbd3fe5
Aranya Kumar <axk1067@case.edu>
2020-10-28 20:38:59 -0400

    removed comments

--- video_stream_test.py
+++ video_stream_test.py
@@ -24,20 +24,3 @@ class Video_Stream_Test:
     def frames(self):
         for i in range(0, 5, 1):
             self.frame = cv2.VideoCapture(index_of_select)
-
-
-#while(True):
-    # Capture frame-by-frame
-    #ret, frame = cap.read()
-
-    # Our operations on the frame come here
-    #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    # Display the resulting frame
-    #cv2.imshow('frame',gray)
-    #if cv2.waitKey(1) & 0xFF == ord('q'):
-        #break
-
-# When everything done, release the capture
-#cap.release()
-#cv2.destroyAllWindows()
