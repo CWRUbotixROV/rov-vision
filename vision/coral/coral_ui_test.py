df0693b55258bfcdf9e667a9833971f54142197f
ryankarpuszka <rykarp5@gmail.com>
2020-12-17 17:59:39 -0500

    Add methods to get images from the images directory

--- vision/coral/coral_ui_test.py
+++ vision/coral/coral_ui_test.py
@@ -1,7 +1,8 @@
 import cv2
 from vision.coral.coral_ui import Coral
+from vision.images import get_image
 
-test_image = cv2.imread("../images/coral/1/1.jpg", 1)
+test_image = get_image("coral", "1", "1.jpg")
 
 test = Coral(test_image)
 test.crop_before_image(test_image)
