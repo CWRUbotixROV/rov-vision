9b2aa24c0e27fecd8e08621f528ec734b39656a4
CoolioYo <georgiacmartinez@gmail.com>
2020-12-24 01:44:39 -0600

    Started square detection

--- vision/grid_map/grid_map.py
+++ vision/grid_map/grid_map.py
@@ -50,6 +50,64 @@ def image_stitching():
         print("Error during stitching")
 
 
+def empty(a):
+    pass
+
+
+def get_contours(img, img_contour):
+    contours, hierarchy = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+
+    for cnt in contours:
+        area = cv2.contourArea(cnt)
+        if area > 1000:
+            cv2.drawContours(img_contour, cnt, -1, (255, 0, 255), 7)
+            peri = cv2.arcLength(cnt, True)
+            approx = cv2.approxPolyDP(cnt, .02 * peri, True)
+
+
+def find_squares(video):
+
+    cv2.namedWindow("Trackbar")
+    cv2.resizeWindow("Trackbar", 640, 240)
+    cv2.createTrackbar("Thresh1", "Trackbar", 87, 255, empty)
+    cv2.createTrackbar("Thresh2", "Trackbar", 255, 255, empty)
+
+    while video.isOpened():
+        ret, frame = video.read()
+
+        if not ret:
+            break
+
+        blur = cv2.GaussianBlur(frame, (7, 7), 1)
+        gray = cv2.cvtColor(blur, cv2.COLOR_BGR2GRAY)
+
+        thresh1 = cv2.getTrackbarPos("Thresh1", "Trackbar")
+        thresh2 = cv2.getTrackbarPos("Thresh2", "Trackbar")
+
+        canny = cv2.Canny(blur, thresh1, thresh2)
+
+        lines = np.zeros_like(frame)
+        draw_lines(lines, canny)
+
+        kernel = np.ones((5, 5))
+        lines = cv2.dilate(lines, kernel, iterations=1)
+        lines = cv2.cvtColor(lines, cv2.COLOR_BGR2GRAY)
+
+        contours = np.zeros_like(frame)
+        get_contours(lines, contours)
+
+        # cv2.imshow("frame", frame)
+        # cv2.imshow("canny", canny)
+        cv2.imshow("lines", lines)
+        cv2.imshow("contours", contours)
+
+        if cv2.waitKey(25) & 0xFF == ord('q'):
+            break
+
+    video.release()
+    cv2.destroyAllWindows()
+
+
 def start_mapping(video):
     """Maps video and displays video with lines drawn on
     For example: 'start_mapping("get_video("transect", "transect.MOV")")'"""
@@ -88,7 +146,8 @@ def start_mapping(video):
             frame_num += 1
 
         # Displaying the videos
-        cv2.imshow("frame", frame)
+        # cv2.imshow("frame", frame)
+        cv2.imshow("red", r_mask)
 
         if cv2.waitKey(25) & 0xFF == ord('q'):
             break
--- vision/grid_map/grid_map_test.py
+++ vision/grid_map/grid_map_test.py
@@ -4,17 +4,20 @@ from vision.images import *
 
 video = get_video("transect", "transect.MOV")
 
+# Tests square detection
+find_squares(video)
+
 # Tests color and line detection
-start_mapping(video)
+# start_mapping(video)
 
 # Tests image stitching
-image_stitching()
+# image_stitching()
 
 # Check if stitched_img exists
-stitched_img = get_image("transect", "stitched_img.jpg")
-
-if stitched_img is not None:
-    # Use k means color clustering on stitched_img
-    get_colors(stitched_img, 20)
-else:
-    print("Could not find stitched image")
+# stitched_img = get_image("transect", "stitched_img.jpg")
+#
+# if stitched_img is not None:
+#     # Use k means color clustering on stitched_img
+#     get_colors(stitched_img, 20)
+# else:
+#     print("Could not find stitched image")
