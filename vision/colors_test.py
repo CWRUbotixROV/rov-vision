c058bd591f0dddb82011621d2ecd582b9d7fe134
Ryan Karpuszka <rykarp5@gmail.com>
2021-01-13 23:27:09 -0500

    Change imshow to show_debug in colors.py

--- vision/colors_test.py
+++ vision/colors_test.py
@@ -1,6 +1,9 @@
 from vision.colors import *
-from vision.images import *
+from vision.images import get_image
+from vision import config
+
+config.debug = True
 
 image = get_image("coral", "1", "1.jpg")
 
-get_colors(image, 10)
+print(get_colors(image, 10))
