bdb89ac6c7e4cd5772854ece507e7ba46d62c5a1
Aranya Kumar <axk1067@case.edu>
2021-02-20 11:08:35 -0500

    Detects lines on frames of video

---
+++ vision/line_detection/line_detection.py
@@ -0,0 +1,60 @@
+import cv2
+import numpy as np
+from vision import images
+import os
+
+def draw_lines():
+    lines = cv2.HoughLinesP(mask, 1, np.pi/180, 100, minLineLength=150, maxLineGap=100)
+
+    if lines is not None:
+        for i in range(len(lines)):
+            line = lines[i]
+
+            x1, y1, x2, y2 = line.reshape(4)
+            cv2.line(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+
+def detect_blue_poles(video):
+    frame_num = 0
+    count = 0
+
+    images.clear_folder("transect", "frames")
+
+    while video.isOpened():
+        ret, frame = video.read()
+
+        if not ret:
+            break
+
+        hsv = cv2.cvtColor(frame, cv2.color.COLOR_BGR2HSV)
+
+        lower_bound_red = []
+        upper_bound_red = []
+
+        lower_bound_blue = []
+        upper_bound_blue = []
+
+        red_mask = cv2.inRange(hsv, lower_bound_red, upper_bound_red)
+        blue_mask = cv2.inRange(hsv, lower_bound_blue, upper_bound_blue)
+
+        lines = np.zeros_like(frame)
+        draw_lines(frame, blue_mask)
+
+    # Get frame every few frames for image stitching
+        if count % 20 == 0:
+            cv2.imwrite(get_folder("transect", "frames") + "/%d.jpg" % frame_num, frame)
+            frame_num += 1
+        count += 1
+
+        # Displaying the videos
+        # cv2.imshow("lines", lines)
+        show_debug(frame, name="frame", wait=False)
+
+        if cv2.waitKey(25) & 0xFF == ord('q'):
+            break
+
+    video.release()
+    cv2.destroyAllWindows()
+
+
+    
+
---
+++ vision/line_detection/line_detection_test.py
@@ -0,0 +1,9 @@
+from vision.line_detection.line_detection import *
+from vision.colors import *
+from vision.images import *
+
+config.debug = True
+
+video = get_video("rov-vision\images\transect\1", "1.mp4")
+
+detect_blue_poles(video)
